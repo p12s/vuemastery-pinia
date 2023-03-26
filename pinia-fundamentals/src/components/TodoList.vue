96592188eaee1ede75b243912bbc8329a9212b9d
p12s <working-tam@yandex.com>
2023-03-26 22:00:54 +0300

    Added the to-do service as example of pinia fundamentals

---
+++ pinia-fundamentals/src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { useTodoListStore } from '@/stores/useTodoListStore'
+import { storeToRefs } from 'pinia'
+
+const store = useTodoListStore();
+
+// storeToRefs lets todoList keep reactivity:
+const { todoList } = storeToRefs(store)
+
+// destructuring action method doesn't require using storeToRefs:
+const { toggleCompleted, deleteTodo } = store
+
+</script>
+
+<template>
+  <div v-for="todo in todoList" :key="todo.id" class="list">
+    <div class="item">
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <div>
+        <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+        <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+.list {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
+.completed {
+  text-decoration: line-through;
+}
+</style>
